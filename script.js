a4ea6a1d9e3d78ab9e5b6b978d6e9d1ba6ef1a3c
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2025-01-13 14:22:32 -0500

    refractors the code and saves mode state top local storage

--- script.js
+++ script.js
@@ -36,9 +36,11 @@ function imageMode(mode) {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
@@ -46,3 +48,16 @@ function switchTheme(event) {
 
 //event listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+//check local storage for theme 
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    //save the theme state in the browser
+    document.documentElement.setAttribute('data-theme', currentTheme); 
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
