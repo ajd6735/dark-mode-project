d0ea0d334b3dad01534125d2fa811382ed73a2cb
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2025-01-13 13:03:20 -0500

    changes the navigation bar to dark/light mode

--- script.js
+++ script.js
@@ -6,14 +6,32 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.color = 'rgb(255 255 255 / 100%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+}
 
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.color = 'rgb(0 0 0 / 75%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+}
 
 //switch theme dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 
