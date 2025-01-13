31525eeef78331ec31036a093e9ced094b0bc390
ajd6735 <117790810+ajd6735@users.noreply.github.com>
2025-01-13 13:45:03 -0500

    refractors the code in js

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
+const light = true;
+const dark = false;
 
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.color = 'rgb(255 255 255 / 100%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
 }
 
 function lightMode() {
@@ -20,8 +22,14 @@ function lightMode() {
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.color = 'rgb(0 0 0 / 75%)';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+function imageMode(mode) {
+    image1.src = `img/undraw_proud_coder_${mode}.svg`;
+    image2.src = `img/undraw_feeling_proud_${mode}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
 }
 
 //switch theme dynamically
